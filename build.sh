73551346ff390dfd2f449af0ba34c847e7c76b25
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-03-23 22:52:13 +0700

    upstream 2.2

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ KERNEL_DIR=$PWD
 REPACK_DIR=$KERNEL_DIR/zip
 OUT=$KERNEL_DIR/out
 ZIP_NAME="$VERSION"-"$DATE"
-VERSION="mido-2.1-OandP"
+VERSION="mido-2.2-OandP"
 DATE=$(date +%Y%m%d-%H%M)
 
 export KBUILD_BUILD_USER=reza-adi-pangestu
