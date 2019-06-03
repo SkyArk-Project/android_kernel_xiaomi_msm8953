8c4b01fa875d4486c0386e26fe1c9b98d19ab9bf
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-06-03 14:33:56 +0700

    update anykernel 3

--- zip/anykernel.sh
+++ zip/anykernel.sh
@@ -1,31 +1,31 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SkyArk kernel for Xiaomi Redmi Note 4(X)
+kernel.string=SkyArk Kernel By Reza Adi P
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=mido
 device.name2=redmi note 4
 device.name3=Redmi Note 4
 device.name4=Redmi Note 4x
-supported.sdk1=27
-supported.sdk2=28
+device.name5=
+supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
-ramdisk_compression=gz;
+ramdisk_compression=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
 
 
 ## AnyKernel file attributes
@@ -33,15 +33,19 @@ ramdisk_compression=gz;
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
+
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
+# Remove CAF Boost Framework cuz CAF is a hoe
 mount -o rw,remount -t auto /vendor >/dev/null;
-rm -rf vendor/etc/perf;
+mount -o rw,remount -t auto /cust >/dev/null;
+rm -rf /vendor/etc/perf;
+rm -rf /cust/etc/perf;
 mount -o ro,remount -t auto /vendor >/dev/null;
-
+mount -o ro,remount -t auto /cust >/dev/null;
 
 # init.rc
 insert_line init.rc 'SkyArk' after 'import /init.\${ro.zygote}.rc' 'import /init.SkyArk.rc';
@@ -49,5 +53,5 @@ insert_line init.rc 'SkyArk' after 'import /init.\${ro.zygote}.rc' 'import /init
 # end ramdisk changes
 
 write_boot;
-
 ## end install
+
